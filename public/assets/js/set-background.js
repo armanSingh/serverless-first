06962bb90a8743d9869a1607f24797bbf7686414
Arman Singh 👌 <armsingh@amazon.com>
2017-11-11 22:06:31 +0530

    Adding boiler plate code for Guess game.

--- public/assets/js/set-background.js
+++ public/assets/js/set-background.js
@@ -1,3 +1,19 @@
-var idx = Math.floor((new Date().getHours()));
-var body = document.getElementsByTagName("body")[0];
-body.className = "heaven-" + idx;
+function evaluateMe() {
+    var number = document.getElementById('num'), input = number.value;
+    console.log(evaluateMe.staticNumGenerated)
+    var bar = evaluateMe.staticNumGenerated
+    var outputString = "Random number to be guessed is: " + bar
+    if(input >= 0 && input <= 100) {
+        if(input == bar){
+             outputString += " Voilla!! You guessed it right! :D";
+        } else if(input < bar) {
+            outputString += document.getElementById("response").innerHTML = " You gotta guess higher! Keep trying.";
+        } else {
+            outputString += document.getElementById("response").innerHTML = " You gotta guess something lower! Keep trying.";
+        }
+    }
+
+    document.getElementById("response").innerHTML = outputString;
+}
+evaluateMe.staticNumGenerated = Math.floor(Math.random()*100);
+
