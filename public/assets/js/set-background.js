5514c5277c0dc6ae296eb129bd7ea3232b77c84d
Arman Singh 👌 <armsingh@amazon.com>
2017-11-12 12:38:56 +0530

    V1.0 Guess Me

--- public/assets/js/set-background.js
+++ public/assets/js/set-background.js
@@ -1,19 +1,24 @@
 function evaluateMe() {
     var number = document.getElementById('num'), input = number.value;
-    console.log(evaluateMe.staticNumGenerated)
-    var bar = evaluateMe.staticNumGenerated
-    var outputString = "Random number to be guessed is: " + bar
     if(input >= 0 && input <= 100) {
-        if(input == bar){
-             outputString += " Voilla!! You guessed it right! :D";
-        } else if(input < bar) {
-            outputString += document.getElementById("response").innerHTML = " You gotta guess higher! Keep trying.";
+        if(input == evaluateMe.staticNumGenerated){
+            evaluateMe.found = 1;
+        } else if(input < evaluateMe.staticNumGenerated) {
+            evaluateMe.lower = input
         } else {
-            outputString += document.getElementById("response").innerHTML = " You gotta guess something lower! Keep trying.";
+            evaluateMe.higher = input
         }
     }
 
-    document.getElementById("response").innerHTML = outputString;
+    if(evaluateMe.found == 0)
+        document.getElementById("response").innerHTML = "You should guess something between: " + evaluateMe.lower + " and " + evaluateMe.higher;
+    else
+        document.getElementById("response").innerHTML = evaluateMe.victoryString;
 }
 evaluateMe.staticNumGenerated = Math.floor(Math.random()*100);
+evaluateMe.lower = 0;
+evaluateMe.higher = 100;
+evaluateMe.found = 0;
+evaluateMe.victoryString =  "You got it right!"
+evaluateMe.message =  "You should guess something between: " + evaluateMe.lower + " and " + evaluateMe.higher;
 
